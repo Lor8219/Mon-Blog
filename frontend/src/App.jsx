bd18d1da001f2d0827cd1103e360fcf3061f33cb
Laure Graves <graveslaure@gmail.com>
2023-05-22 20:34:23 +0200

    nettoyage du projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1 className="text-center text-secondary">My Travel Blog </h1>
     </div>
   );
 }
