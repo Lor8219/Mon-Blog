4b2c554d002378af81505e867941bf0accdf1afa
Laure Graves <graveslaure@gmail.com>
2023-05-22 23:46:31 +0200

    ajout de la logique pour recuperation de l'article

--- frontend/src/pages/OneArticle.jsx
+++ frontend/src/pages/OneArticle.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import articles from "@assets/data.json";
 
 function OneArticle() {
-  return <div>OneArticle</div>;
+  const { id } = useParams();
+  const article = articles.find((art) => art.id === parseInt(id, 10));
+  return <div>OneArticle - {article.title}</div>;
 }
 
 export default OneArticle;
