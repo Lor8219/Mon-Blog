90a2ba65781f05d4aa96eab5cb53767723e0c6ec
Laure Graves <graveslaure@gmail.com>
2023-05-22 22:55:08 +0200

    Integration des datas et style dans articleCard

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -3,7 +3,23 @@ import PropTypes from "prop-types";
 import ArticleType from "../types/article";
 
 function ArticleCard({ article }) {
-  return <div>{article.title}</div>;
+  return (
+    <div className="col-md-4 col-sm-12 d-flex">
+      <div className="border m-2 p-2 rounded-1 shadow-1 flex-fill">
+        <h2 className="text-center">{article.title}</h2>
+        <div className="p-2">
+          <img
+            src={article.image.src}
+            alt={article.image.alt}
+            className="w-100 rounded.1"
+          />
+        </div>
+        <div>
+          <p className="btn btn-secondary w-100">En savoir plus</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ArticleCard.propTypes = {
