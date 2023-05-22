de8c41c2d705de48b963d48465a2930914399191
Laure Graves <graveslaure@gmail.com>
2023-05-22 23:17:24 +0200

    creation de la page vierge OneArticle plus mise en place route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Articles from "@pages/Articles";
+import OneArticle from "@pages/OneArticle";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <h1 className="text-center text-secondary">My Travel Blog </h1>
         <Routes>
           <Route path="/" element={<Articles />} />
+          <Route path="/articles/:id" element={<OneArticle />} />
         </Routes>
       </div>
     </BrowserRouter>
