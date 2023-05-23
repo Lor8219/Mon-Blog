8800b21c6d03397068f2779bb8cd80e937a829e4
Laure Graves <graveslaure@gmail.com>
2023-05-23 16:00:35 +0200

    mise en place de la balise title et description dans le head via helmet

--- frontend/src/pages/OneArticle.jsx
+++ frontend/src/pages/OneArticle.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
 import articles from "@assets/data.json";
 
 function OneArticle() {
   const { id } = useParams();
   const article = articles.find((art) => art.id === parseInt(id, 10));
-  return <div>OneArticle - {article.title}</div>;
+  return (
+    <div className="container">
+      <Helmet>
+        <title>{article.title} - Travel Blog</title>
+        <meta name="description" content={article.resume} />
+      </Helmet>
+      <div className="row mx-4 my-4">
+        <h2 className="text-center text-secondary">{article.title}</h2>
+        <h4 className="text-center">{article.subtitle}</h4>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src={article.image.src}
+            alt={article.image.alt}
+            className="w-100 rounded-2 shadow-sm"
+          />
+          <h6 className="text-center">{article.resume}</h6>
+        </div>
+        <div className="col-6">
+          <p>{article.text}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OneArticle;
