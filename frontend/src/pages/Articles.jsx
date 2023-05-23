16ea0de8329b9dab298a030344cb17bd78782d32
Laure Graves <graveslaure@gmail.com>
2023-05-23 16:40:56 +0200

    ajout helmet dans page Articles

--- frontend/src/pages/Articles.jsx
+++ frontend/src/pages/Articles.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import ArticleCard from "@components/ArticleCard";
 import articles from "@assets/data.json";
 
 function Articles() {
   return (
     <section className="container">
+      <Helmet>
+        <title>Evadez-Vous - Travel Blog</title>
+        <meta
+          name="description"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium libero iste debitis fugit, nihil recusandae aut quisquam tempore reprehenderit accusantium quasi ad officiis officia illo tempora perferendis suscipit excepturi cum."
+        />
+      </Helmet>
       <div className="row">
         {articles.map((article) => (
           <ArticleCard article={article} />
