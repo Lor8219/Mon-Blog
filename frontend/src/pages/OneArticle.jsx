921f88b2a49ff4e3b6bb15bf99e4b977ca40213d
Laure Graves <graveslaure@gmail.com>
2023-06-01 22:20:01 +0200

    Intégration de la requete pour recuperer les données du back, adaptation du jsx

--- frontend/src/pages/OneArticle.jsx
+++ frontend/src/pages/OneArticle.jsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import articles from "@assets/data.json";
+// import articles from "@assets/data.json";
 
 function OneArticle() {
   const { id } = useParams();
-  const article = articles.find((art) => art.id === parseInt(id, 10));
+
+  const [article, setArticle] = useState();
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/articles/${id}`)
+      .then((res) => res.json())
+      .then((art) => setArticle(art))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="container">
-      <Helmet>
-        <title>{article.title} - Travel Blog</title>
-        <meta name="description" content={article.resume} />
-      </Helmet>
-      <div className="row mx-4 my-4">
-        <h2 className="text-center text-secondary">{article.title}</h2>
-        <h4 className="text-center">{article.subtitle}</h4>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src={article.image.src}
-            alt={article.image.alt}
-            className="w-100 rounded-2 shadow-sm"
-          />
-          <h6 className="text-center">{article.resume}</h6>
-        </div>
-        <div className="col-6">
-          <p>{article.text}</p>
-        </div>
-      </div>
+      {article && (
+        <>
+          <Helmet>
+            <title>{article.title} - Travel Blog</title>
+            <meta name="description" content={article.resume} />
+          </Helmet>
+          <div className="row mx-4 my-4">
+            <h2 className="text-center text-secondary">{article.title}</h2>
+            <h4 className="text-center">{article.subtitle}</h4>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <img
+                src={article.src}
+                alt={article.alt}
+                className="w-100 rounded-2 shadow-sm"
+              />
+              <h6 className="text-center">{article.resume}</h6>
+            </div>
+            <div className="col-6">
+              <p>{article.text}</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
